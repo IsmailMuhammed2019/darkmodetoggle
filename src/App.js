9183fef63b11e452172390278d2f8fdb73a4c93e
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-05 15:56:12 +0100

    update image folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Main from '../components/Main'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import './App.css'
 
 export default function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar(props) {
   return (
     <nav className={props.darkMode ? 'dark' : ''}>
-      <img className='nav--logo_icon' src='./images/react-icon-small.png' />
+      <img className='nav--logo_icon' src='react-icon-small.png' alt=''/>
       <h3 className='nav--logo_text'>ReactFacts</h3>
 
       <div className='toggler'>
