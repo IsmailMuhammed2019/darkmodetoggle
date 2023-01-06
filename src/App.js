b0272046f2454ec6623c30733f52203f3fcb62f1
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-06 15:46:15 +0100

    update componet props

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import Main from './components/Main'
 import './App.css'
 
 export default function App() {
-  const [darkMode, setDarkMode] = React.useState(true)
-  
-    function toggleDarkMode () {
-      setDarkMode(prevMode => !prevMode)
-    }
-  return (
+  const[darkMode, setDarkMode] = React.useState(false)
 
+  function toggleDarkMode () {
+    setDarkMode(prevMode => !prevMode)
+  }
+  return (
     <div className='container'>
       <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Main darkMode={darkMode} />
+      <Main darkMode={darkMode}/>
     </div>
   )
 }
